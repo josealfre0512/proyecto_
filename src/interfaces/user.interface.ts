0a46ba09caa303c2744fa23e595ed813e11aaf72
Jose Alfredo <josealfre594@gmail.com>
2026-05-25 22:02:25 -0400

    chore: limpieza total de la rama main - dejar en 0

--- src/interfaces/user.interface.ts
+++
@@ -1,8 +0,0 @@
-export interface IUser {
-  _id?: string;
-  name: string;
-  email: string;
-  password?: string;
-  createdAt?: Date;
-  updatedAt?: Date;
-}
--- src/routes/auth.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import { register, login } from '../controllers/auth.controller';
-const router = Router();
-
-// Definición de endpoints públicos para autenticación
-router.post('/register', register);
-router.post('/login', login);
-
-export default router;
